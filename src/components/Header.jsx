d83b4b7354fa7b07f3cd0f2b5c7b7cc306d0f259
Stepan Voroncov <svoron87@gmail.com>
2021-01-23 15:36:49 +0300

    ready to deploy

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,6 +9,7 @@ export const Header = () => {
               <Link to="/" className="brand-logo"><i className="material-icons">receipt</i>React Receipt</Link>
               <ul id="nav-mobile" className="right hide-on-med-and-down">
                 <li><Link to="/">Home</Link></li>
+                <li><a href="https://github.com/C1rfa/reactRecipe">Repository</a></li>
             </ul>
             </div>
         </nav>
